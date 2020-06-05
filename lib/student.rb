41be74ee82187e484defc103e3d7a2a596e4a3a5
mayalexandera <maya.blevins@gmail.com>
2020-06-05 19:09:26 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative './user.rb'
+class Student < User
+  attr_reader :knowledge
+  def initialize
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
 
 end
